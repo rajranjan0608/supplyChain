867eb8472fc0d698e1ddc0c8848cc2e016e90418
realnimish <46227078+realnimish@users.noreply.github.com>
2020-01-16 17:33:58 +0530

    Tender.sol Updated
    Whole code replaced.

--- contracts/Tender.sol
+++ contracts/Tender.sol
@@ -1,59 +1,96 @@
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity >=0.5.0 <=0.6.0;
 pragma experimental ABIEncoderV2;
 
 contract Tender {
+	uint tenderID;
+	Status status;
+	uint endTime;
+	address winner;
+	string name;
+	string desc;
+	string[] req;
+	Bid[] bids;
 
-    uint id;
-    string name;
-    string desc;
-    uint256 timestamp;
-    uint256 endTime;
-    address winner;
+	enum Status {open,shipping,payment,closed,cancel}
 
-    enum Status {OPEN, CLOSED}  // enum
-    Status status = Status.OPEN;
+	struct Bid {
+		address bidder;
+		string[] data;
+	}
 
-    uint bidId = 0;
+	constructor(uint _tenderID, uint _bidTime, string[] memory _req) public {
+		tenderID = _tenderID;
+		endTime = now + _bidTime;
+		name = _req[0];
+		desc = _req[1];
+		for(uint i = 2; i < _req.length; ++i)
+		{
+			req.push(_req[i]);
+		}
+	}
 
-    struct Bid {
-        address bidder;
-        string[] data;
-    }
+	event bidAlert(uint _tenderID, uint bidNumber, address bidder);
+	event newEndTime(uint _tenderID, uint _newEndTime);
+	event Winner(uint _tenderID, uint bidNumber, address indexed winner)
 
-    mapping (uint => Bid) public bids;      // array or map
-    mapping (address => string) public userType;
+	modifier onlyAdmin() {}
+	modifier onlyBidder() {}
 
-    modifier onlyBidder(address _user) {
-        require(userType[_user] == "Bidder", "User is not a Bidder");
-        _;
-    }
+	modifier beforeEndTime() {
+		require(now <= endTime);
+		_;
+	}
 
-    modifier onlyAdmin(address _user) {
-        require(userType[_user] == "Admin", "User is not an Admin");
-        _;
-    }
+	modifier afterEndTime() {
+		require(now > endTime);
+		_;
+	}
 
-    modifier beforeEndTime() {
-        require(status == Status.OPEN, "Tender has been closed");
-        _;
-    }
 
-    function makeBid(string[] memory _data) public onlyBidder(msg.sender) beforeEndTime {
-        bidId++;
-        bids[bidId] = Bid(msg.sender, _data);
-    }
+	function makeBid(string[] calldata _Data) external onlyBidder beforeEndTime {
+		Bid newBid = Bid(msg.sender, _Data);
+		bids.push(newBid);
+		emit bidAlert(tenderID, bids.length, msg.sender);
+	}
 
-    function endBidding() public onlyAdmin(msg.sender) beforeEndTime {
-        timestamp = now;
-        status = Status.CLOSED;
-    }
+	function extendEndTime(uint _val) external onlyAdmin beforeEndTime {
+		require(_val > 0, "endTime cannot be extended by non-positive value");
+		endTime += _val;
+		emit newEndTime(tenderID,endTime);
+	}
 
-    function extendEndTime(uint256 _timestamp) public onlyAdmin(msg.sender) {
-        endTime = _timestamp;
-    }
+	function declareWinner(uint _bidNumber) external onlyAdmin afterEndTime {
+		require(status == Status.open, "Winner already selected");
+		require(_bidNumber >=1 && _bidNumber <= bids.length,"Invalid Bid Number");
+		winner = bids[_bidNumber].bidder;
+		status = Status.shipping;
+		emit Winner(tenderID, _bidNumber, winner);
+	}
 
-    function declareWinner() {
+	function displayTenderDetails() external view returns(uint,uint,string,string,string,string[] memory) {
+		return (tenderID, endTime, name, desc, currentStatus(), req);
+	}
 
-    }
+	// Helper Function
+	function currentStatus() internal view returns(string)
+	{
+		string memory statusType[] = ["Open","Shipping","Payment","Closed","Cancel"];
+		return statusType[status];
+	}
+
+	function isActive() external view returns(uint8) {
+		if (status < status.closed) return 1;
+		return 0;
+	}
+
+	function getBidByID(uint _bidNumber) external view returns(string[] memory) {
+		require(_bidNumber >=1 && _bidNumber <= bids.length,"Invalid Bid Number");
+		require(bids[_bidNumber].bidder == msg.sender || USER_TYPE[msg.sender] == privilege.admin);
+		return bids[_bidNumber].data;
+	}
+
+	//function getMyBids() external view onlyBidder returns() {}
+	//function getAllBids() external view onlyAdmin returns() {}
+
 
 }
