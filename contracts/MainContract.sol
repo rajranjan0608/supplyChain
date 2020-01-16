df236d58a943359a9fabcfd2d20d01f8e062120d
realnimish <46227078+realnimish@users.noreply.github.com>
2020-01-16 17:34:25 +0530

    Update MainContract.sol

--- contracts/MainContract.sol
+++ contracts/MainContract.sol
@@ -1,51 +1,78 @@
-pragma solidity ^0.6.0;
+pragma solidity >=0.4.21 <=0.6.0;
+pragma experimental ABIEncoderV2;
+
+// contract Tender {
+//     constructor(uint _tenderID, string[] memory _requirments) public {}
+// }
 
 contract MainContract {
 
-	enum userChoice {normal,bidder,admin};
-	mapping(address => userChoice) userType;
-	mapping(uint256 => address) Tenders;	// Key := tenderID
-	uint256[] tenderID;
+	enum privilege { normal, bidder, admin}	// Three Types of user
+	mapping(address => privilege) public userType; // Stores the privilege of users
+	mapping(uint => address) Tenders;	// tenderID => tenderContractAddress
+	uint[] tenderID;	// Stores the unique tender ID deployed
 
-	constructor(){
-		userType[msg.sender] = userChoice.admin;
+	constructor() public {
+		userType[msg.sender] = privilege.admin;
 	}
 
-
 	modifier onlyAdmin() {
-		require( userType[msg.sender] == userChoice.admin , "Only Admin can call this function.");
+		require(userType[msg.sender] == privilege.admin, "Only Admin can call this function.");
 		_;
 	}
 
-
-	function approveBidder(address[] memory _newBidder) external onlyAdmin {
-
+	function approveBidder(address[] calldata _newBidder) external onlyAdmin {
 		for(uint i = 0; i < _newBidder.length ; ++i)
 		{
-			userType[ _newBidder[i] ] = userChoice.bidder;
+			userType[_newBidder[i]] = privilege.bidder;
 		}
 	}
 
-
-	function approveAdmin(address[] memory _newAdmin) external onlyAdmin {
-
+	function approveAdmin(address[] calldata _newAdmin) external onlyAdmin {
 		for(uint i = 0; i < _newAdmin.length ; ++i)
 		{
-			userType[ _newAdmin[i] ] = userChoice.admin;
+			userType[_newAdmin[i]] = privilege.admin;
 		}
 	}
 
+	/// @dev Creates a new tender contract
+	function createTender(uint _tenderID, uint _bidTime, string[] calldata _req) external onlyAdmin returns(address) {
+		require(Tenders[_tenderID ] == address(0), "A Tender already exists with the given tender ID");
 
-	function createTender(uint256 _tenderID, string[] memory _requirments) external onlyAdmin returns(address) {
-		require(Tenders[ _tenderID ] == address(0), "A Tender already exists with the given tender ID");
-		Tenders[ _tenderID ] = Tender( _tenderID, _requirments);
-		tenderID.push( _tenderID );
-		return Tenders[ _bidID ];
+		Tender tender = new Tender(_tenderID, _bidTime, _req);
+		Tenders[_tenderID] = address(tender);
+		tenderID.push(_tenderID);
+		return Tenders[_tenderID];
 	}
 
+	/// returns the contract address of the tender with the given tenderID
+	function getTender(uint _tenderID) external view returns(address) {
+		require( Tenders[_tenderID] != address(0), "No Tender Found!");
+		return Tenders[_tenderID];
+	}
 
-	function getTender(uint256 _tenderID) external view returns(address) {
-		require( Tenders[ _tenderID ] != address(0), "No Tender Found!");
-		return Tender[ _tenderID ];
+	function getAllTenders() external view returns(address[] memory) {
+	    address[] memory tender = new address[](tenderID.length);
+		for(uint i = 0; i < tenderID.length; ++i)
+		{
+			tender[i] = Tenders[tenderID[i]];
+		}
+		return tender;
+	}
+
+	/// @param
+	/// _mode := 0 To return Active Tenders
+	/// _mode := 1 To return Closed Tenders
+	/// @dev Can be made more flexible
+	function getTendersByStatus(uint8 _mode) external view returns(address[] memory) {
+		address[] memory tender = new address[](tenderID.length);
+		for(uint i = 0, uint idx = 0; i < tenderID.length; ++i)
+		{
+			if ( (Tender(Tenders[tenderID[i]]).isActive()^_mode) == 1)	// isActive() to be implemented!!!
+			{
+				tender[idx++] = Tenders[tenderID[i]];
+			}
+		}
+		return tender;
 	}
 }
