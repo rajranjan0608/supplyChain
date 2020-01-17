1fedb9304008a84451af14019f14125e7320cc60
realnimish <46227078+realnimish@users.noreply.github.com>
2020-01-17 18:23:22 +0530

    Simplified Tender model
    For short term goals The tender only opens for one product.
    
    Note: Found out that we were running high on gas which caused the all problem. Prev approach can be applied later on.

--- contracts/Tender.sol
+++ contracts/Tender.sol
@@ -1,86 +1,84 @@
 pragma solidity >=0.4.0 <0.6.0;
 pragma experimental ABIEncoderV2;
-import "./MainContract.sol";
+import "./User.sol";
 
 contract Tender {
 	uint tenderID;
-	Status status;
 	uint public endTime;
-	address factoryAddress;
+	Status status;
 	string title;
 	string desc;
-	string[] req;
+
+	string item;
+	uint qty;
+
 	Bid public bestBid;
 	Bid[] bids;
+	User database;
 
 	enum Status {open,shipping,payment,closed,cancel}
 
 	struct Bid {
 		address bidder;
-		string[] data;      // possible error
-	}
-
-	constructor(uint _tenderID, uint _bidTime, string[] memory _req) public {
-		tenderID = _tenderID;
-		endTime = now + _bidTime;
-		factoryAddress = msg.sender;	// Verify
-		title = _req[0];
-		desc = _req[1];
-// 		for(uint i = 2; i < _req.length; ++i)
-// 		{
-// 			req.push(_req[i]);
-// 		}
+		uint price;
 	}
 
 	event bidAlert(uint _tenderID, uint bidNumber, address bidder);
 	event newEndTime(uint _tenderID, uint _newEndTime);
-	event Winner(uint _tenderID, uint bidNumber, address indexed winner);
+	event Winner(uint _tenderID, address indexed winner, uint _lowestPrice);
+
+	constructor(uint _tenderID, uint _bidTime,string memory _title,string memory _desc, address _database, string memory _item, uint _qty) public {
+		tenderID = _tenderID;
+		endTime = now + _bidTime;
+		database = User(_database);
+		title = _title;
+		desc = _desc;
+		item = _item;
+		qty = _qty;
+	}
 
 	modifier onlyAdmin() {
-		MainContract factory = MainContract(factoryAddress);
-		require(uint8(factory.userType(msg.sender)) == 2, "Only Admin can call this function.");
+		require(database.getUserType(msg.sender) == 2, "Only Admin can call this function");
 		_;
 	}
-	
+
 	modifier onlyBidder() {
-		MainContract factory = MainContract(factoryAddress);
-		require(uint8(factory.userType(msg.sender)) == 1, "Only Bidder can call this function.");
+		require(database.getUserType(msg.sender) == 1, "Only bidder can call this function");
 		_;
 	}
 
-	modifier beforeEndTime() {
-		require(now <= endTime);
+	modifier beforeEndTime() {			// Adding Text leads to deployment error
+		require(now < endTime);
 		_;
 	}
 
-	modifier afterEndTime() {
+	modifier afterEndTime() {			// Adding Text leads to deployment error
 		require(now > endTime);
 		_;
 	}
-
-    /// Problem detected
-// 	function makeBid(string[] calldata _Data) external onlyBidder beforeEndTime {
-
-// 		//bids.push( Bid(msg.sender, _Data) );
-// 		emit bidAlert(tenderID, bids.length, msg.sender);
-// 	}
-
-	function extendEndTime(uint _val) external onlyAdmin beforeEndTime {
-		require(_val > 0, "endTime cannot be extended by non-positive value");
-		endTime += _val;
-		emit newEndTime(tenderID,endTime);
+	
+	function isActive() external view returns(uint8) {
+		if (status < Status.closed) return 1;
+		return 0;
 	}
 
-	function declareWinner(uint _bidNumber) external onlyAdmin afterEndTime {
-		require(status == Status.open, "Winner already selected");
+	/// Simple one product model
+	/// @param _price -> overall bid
+	function makeBid(uint _price) external onlyBidder beforeEndTime {
+        require(_price > 0,"Price cannot be non-positive");
+        Bid memory myBid = Bid(msg.sender, _price);
+        bids.push(myBid);
+		emit bidAlert(tenderID, bids.length, msg.sender);
+	}
+
+	function getBidByID(uint _bidNumber) external view returns(address,uint) {
 		require(_bidNumber >=1 && _bidNumber <= bids.length,"Invalid Bid Number");
-		bestBid = bids[_bidNumber];
-		status = Status.shipping;
-		emit Winner(tenderID, _bidNumber, bestBid.bidder);
-	}
-
-	function displayTenderDetails() external view returns(uint,uint,string memory,string memory,string memory,string[] memory) {
-		return (tenderID, endTime, title, desc, currentStatus(), req);
+		if (database.getUserType(msg.sender) != 2)
+		{
+			require(bids[_bidNumber-1].bidder == msg.sender,"Not Allowed");
+		}
+		Bid memory bid = bids[_bidNumber-1];
+		return(bid.bidder,bid.price);
 	}
 
 	// Helper Function
@@ -89,21 +87,88 @@ contract Tender {
 		return statusType[uint(status)];
 	}
 
-	function isActive() external view returns(uint8) {
-		if (status < Status.closed) return 1;
-		return 0;
+	/// @dev Maybe separate req[] to another function
+	function displayTenderDetails() external view returns(uint,uint,string memory,string memory,string memory,string memory, uint) {
+		return (tenderID, endTime, title, desc, currentStatus(), item, qty);
 	}
 
-	function getBidByID(uint _bidNumber) external view returns(string[] memory) {
-		require(_bidNumber >=1 && _bidNumber <= bids.length,"Invalid Bid Number");
+	/// Extends the endTime by _val seconds
+	function extendEndTime(uint _val) external onlyAdmin beforeEndTime {
+		require(_val > 0, "endTime cannot be extended by non-positive value");
+		endTime += _val;
+		emit newEndTime(tenderID,endTime);
+	}
 
-		MainContract factory = MainContract(factoryAddress);
-		if (uint8(factory.userType(msg.sender)) != 2)
+	/// Selects the first bid with smallest bid amount
+	function getWinner() external onlyAdmin afterEndTime returns(bool) {
+		require(status == Status.open, "Winner already selected");
+
+		if (bids.length == 0)
 		{
-			require(bids[_bidNumber-1].bidder == msg.sender);
+			status = Status.closed;
+			return false;
 		}
 
-		return bids[_bidNumber-1].data;
+		bestBid = bids[0];
+		for(uint i = 1; i < bids.length; ++i)
+		{
+			if(bestBid.price > bids[i].price)
+			{
+				bestBid = bids[i];
+			}
+		}
+
+		status = Status.shipping;
+		emit Winner(tenderID, bestBid.bidder, bestBid.price);
+		return true;
 	}
 
+	function getAllBids() external view onlyAdmin returns(Bid[] memory)
+	{
+		// Bid[] memory tmpBid = new Bid[](bids.length);
+		// for(uint i = 0; i < bids.length; ++i)
+		// {
+		// 	tmpBid[i] = bids[i];
+		// }
+		// return tmpBid;
+		return bids;
+	}
+
+	function getMyBids() external view onlyBidder returns(uint[] memory){
+		uint[] memory myBids = new uint[](bids.length);
+		uint idx = 0;
+		for(uint i = 0; i < bids.length; ++i)
+		{
+			if(bids[i].bidder == msg.sender)
+			{
+				myBids[idx++] = bids[i].price;
+			}
+		}
+		return myBids;
+	}
 }
+
+	// function addRequirement(string[] calldata _req) external onlyAdmin {
+	// 	require(req.length == 0,"Requirement already Set");
+	// 	require(_req.length >= 3,"Requirement cannot be empty");
+	// 	title = _req[0];
+	// 	desc = _req[1];
+	// 	for(uint i = 2; i < _req.length; i++)
+	// 	{
+	// 	    req.push(_req[i]);
+	// 	}
+	// }
+
+	// function makeBid(string[] memory _Data) public onlyBidder beforeEndTime {
+ //        require(_Data.length > 0,"Bid cannot be empty");
+
+ //        Bid storage newBid = bids[bids.length++];
+ //        newBid.bidder = msg.sender;
+        
+ //        for(uint i = 0; i < _Data.length; ++i)
+ //        {
+ //            newBid.data[newBid.data.length++] = _Data[i];
+ //        }
+
+	// 	emit bidAlert(tenderID, bids.length, msg.sender);
+	// }
