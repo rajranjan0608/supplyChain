d0a3913230d5eeda06ee441fd1fea725f68a71e2
realnimish <46227078+realnimish@users.noreply.github.com>
2020-01-17 18:17:41 +0530

    Create User.sol
    Separated User privilege data as used from two contracts

---
+++ contracts/User.sol
@@ -0,0 +1,26 @@
+pragma solidity >=0.4.0 <0.6.0;
+
+contract User{
+	// 0 -> Normarl User
+	// 1 -> Bidder
+	// 2 -> Admin
+	mapping(address => uint8) userType; // Stores the privilege of users
+
+	constructor(address _creator) public {
+		userType[_creator] = 2;
+		userType[msg.sender] = 2; // Giving MainContract Admin Right too!
+	}
+
+	modifier onlyAdmin(){		// What if contract makes a call?
+		require(userType[msg.sender] == 2);
+		_;
+	}
+
+	function getUserType(address _node) external view returns(uint8) {
+		return userType[_node];
+	}
+
+	function setUserType(address _node,uint8 _val) external onlyAdmin {
+		userType[_node] = _val;
+	}
+}
