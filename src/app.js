5b0d367af72f8b0309beaaf822fa292ad131df63
Sameer Kumar Kushwaha <44394184+kushwahasameerkumar@users.noreply.github.com>
2020-01-17 23:11:25 +0530

    Login API added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+//login API
+var db = require('./db');
+
 const MainContract = require('../build/contracts/MainContract.json');
 
 app.use(express.static("public"));
@@ -12,6 +15,13 @@ app.get('/', (req,res) => {
 app.get('/MainContract', (req,res) => {
     res.send(MainContract);
 });
+
+//login API--------------
+
+var AuthController = require('./auth/AuthController');
+app.use('/api/auth', AuthController);
+
+//-----------------------
 
 app.listen(3000, () => {
     console.log('Server started at 3000');
